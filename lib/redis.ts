e1b32664b482399ac8d0156e0a39c22d40225d7f
wvniekerk1234 <wvniekerk1234@gmail.com>
2025-06-06 11:43:17 +0200

    Create redis.ts

---
+++ lib/redis.ts
@@ -0,0 +1,9 @@
+import { Redis } from '@upstash/redis';
+
+// Create a Redis client using environment variables
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL || '',
+  token: process.env.UPSTASH_REDIS_REST_TOKEN || '',
+});
+
+export default redis;
